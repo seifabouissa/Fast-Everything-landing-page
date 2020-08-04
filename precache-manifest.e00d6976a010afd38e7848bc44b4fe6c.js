6ee0604091c2a033ec37a6e60323c65e3097d00c
seifabouissa <seifeldinashrafabouissa@gmail.com>
2020-08-04 22:24:24 +0200

    Updates

---
+++ precache-manifest.e00d6976a010afd38e7848bc44b4fe6c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e30751a7e494f693dc5f4538ef545c",
+    "url": "/Fast-Everything-landing-page/index.html"
+  },
+  {
+    "revision": "dc25be70521e16dc496f",
+    "url": "/Fast-Everything-landing-page/static/css/main.1e7eed48.chunk.css"
+  },
+  {
+    "revision": "0e666a4d304c52e12623",
+    "url": "/Fast-Everything-landing-page/static/js/2.4beb053a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fast-Everything-landing-page/static/js/2.4beb053a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc25be70521e16dc496f",
+    "url": "/Fast-Everything-landing-page/static/js/main.8e025b22.chunk.js"
+  },
+  {
+    "revision": "65a14d64da9391eafcb2",
+    "url": "/Fast-Everything-landing-page/static/js/runtime-main.e6c4a2ec.js"
+  },
+  {
+    "revision": "9d4b8687bdbbbc7170e3e542ab4c9edc",
+    "url": "/Fast-Everything-landing-page/static/media/BG.9d4b8687.jpg"
+  },
+  {
+    "revision": "29313d1630a3f02dc6d8e7ee7a0a26eb",
+    "url": "/Fast-Everything-landing-page/static/media/BG_Pattern.29313d16.png"
+  },
+  {
+    "revision": "4f33cf2b7bc6bd8c17874de2cc25bb45",
+    "url": "/Fast-Everything-landing-page/static/media/Runner.4f33cf2b.png"
+  }
+]);
